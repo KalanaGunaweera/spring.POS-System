1b82bf693cae40dc26f7afe293b0984a4c8cb57c
KalanaGunaweera <kalanagunaweera94@gmail.com>
2023-06-28 21:58:21 +0530

    NotFound EXCEPTION DONE with Advisor pkg

---
+++ src/main/java/spring/POSSystem/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,16 @@
+package spring.POSSystem.advisor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import spring.POSSystem.exception.NotFoundException;
+import spring.POSSystem.util.StandardResponse;
+
+@RestControllerAdvice
+public class AppWideExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleNotFoundException(NotFoundException e){
+        return new ResponseEntity(new StandardResponse(404,"Error",e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+}
